Pkcs11Interop/Pkcs11Interop

commit 8ea0edef74f4246444d655efc9841ccaf222e13f

OSX library loading issues resolved

src/Pkcs11Interop/Common/NativeMethods.cs
@@ -55,15 +55,25 @@ namespace Net.Pkcs11Interop.Common
         #region Unix
         
         /// <summary>
-        /// Lazy function call binding
+        /// Resolve symbols when they are used
         /// </summary>
         internal const int RTLD_LAZY = 0x00001;
         
         /// <summary>
-        /// Immediate function call binding 
+        /// Immediately resolve all symbols
         /// </summary>
         internal const int RTLD_NOW = 0x00002;
-        
+
+        /// <summary>
+        /// Resolved symbols are not available for subsequently loaded libraries
+        /// </summary>
+        internal const int RTLD_LOCAL = 0x00004;
+
+        /// <summary>
+        /// Resolved symbols are available for subsequently loaded libraries
+        /// </summary>
+        internal const int RTLD_GLOBAL = 0x00008;
+
         /// <summary>
         /// Human readable string describing the most recent error that occurred from dlopen(), dlsym() or dlclose() since the last call to dlerror().
         /// </summary>

src/Pkcs11Interop/Common/UnmanagedLibrary.cs
@@ -22,14 +22,14 @@ namespace Net.Pkcs11Interop.Common
     /// <summary>
     /// Utility class that helps to manage unmanaged dynamic libraries
     /// </summary>
-    internal static class UnmanagedLibrary
+    public static class UnmanagedLibrary
     {
         /// <summary>
         /// Loads the dynamic library
         /// </summary>
         /// <param name='fileName'>Library filename</param>
         /// <returns>Dynamic library handle</returns>
-        internal static IntPtr Load(string fileName)
+        public static IntPtr Load(string fileName)
         {
             if (fileName == null)
                 throw new ArgumentNullException("fileName");
@@ -39,7 +39,7 @@ namespace Net.Pkcs11Interop.Common
             PlatformID platformId = System.Environment.OSVersion.Platform;
             if ((platformId == PlatformID.Unix) || (platformId == PlatformID.MacOSX))
             {
-                libraryHandle = NativeMethods.dlopen(fileName, NativeMethods.RTLD_NOW);
+                libraryHandle = NativeMethods.dlopen(fileName, NativeMethods.RTLD_NOW | NativeMethods.RTLD_LOCAL);
                 if (libraryHandle == IntPtr.Zero)
                 {
                     IntPtr error = NativeMethods.dlerror();
@@ -63,7 +63,7 @@ namespace Net.Pkcs11Interop.Common
         /// Unloads the dynamic library
         /// </summary>
         /// <param name='libraryHandle'>Dynamic library handle</param>
-        internal static void Unload(IntPtr libraryHandle)
+        public static void Unload(IntPtr libraryHandle)
         {
             if (libraryHandle == IntPtr.Zero)
                 throw new ArgumentNullException("libraryHandle");
@@ -93,7 +93,7 @@ namespace Net.Pkcs11Interop.Common
         /// <param name='libraryHandle'>Dynamic library handle</param>
         /// <param name='function'>Function name</param>
         /// <returns>The function pointer</returns>
-        internal static IntPtr GetFunctionPointer(IntPtr libraryHandle, string function)
+        public static IntPtr GetFunctionPointer(IntPtr libraryHandle, string function)
         {
             if (libraryHandle == IntPtr.Zero)
                 throw new ArgumentNullException("libraryHandle");

commit 00f406ce172e640580c959af5fbd46712c3aa798

Custom unix platform detection
Resolves issues introduced by commit
8ea0edef74f4246444d655efc9841ccaf222e13f

src/Pkcs11Interop/Common/NativeMethods.cs
@@ -53,26 +53,26 @@ namespace Net.Pkcs11Interop.Common
         #endregion
          
         #region Unix
-        
-        /// <summary>
-        /// Resolve symbols when they are used
-        /// </summary>
-        internal const int RTLD_LAZY = 0x00001;
-        
+
         /// <summary>
         /// Immediately resolve all symbols
         /// </summary>
-        internal const int RTLD_NOW = 0x00002;
+        internal const int RTLD_NOW_LINUX = 0x2;
 
         /// <summary>
         /// Resolved symbols are not available for subsequently loaded libraries
         /// </summary>
-        internal const int RTLD_LOCAL = 0x00004;
+        internal const int RTLD_LOCAL_LINUX = 0;
 
         /// <summary>
-        /// Resolved symbols are available for subsequently loaded libraries
+        /// Immediately resolve all symbols
         /// </summary>
-        internal const int RTLD_GLOBAL = 0x00008;
+        internal const int RTLD_NOW_MACOSX = 0x2;
+
+        /// <summary>
+        /// Resolved symbols are not available for subsequently loaded libraries
+        /// </summary>
+        internal const int RTLD_LOCAL_MACOSX = 0x4;
 
         /// <summary>
         /// Human readable string describing the most recent error that occurred from dlopen(), dlsym() or dlclose() since the last call to dlerror().

src/Pkcs11Interop/Common/Platform.cs
@@ -1,0 +1,118 @@
+﻿/*
+ *  Pkcs11Interop - Managed .NET wrapper for unmanaged PKCS#11 libraries
+ *  Copyright (c) 2012-2014 JWC s.r.o. <http://www.jwc.sk>
+ *  Author: Jaroslav Imrich <jimrich@jimrich.sk>
+ *
+ *  Licensing for open source projects:
+ *  Pkcs11Interop is available under the terms of the GNU Affero General 
+ *  Public License version 3 as published by the Free Software Foundation.
+ *  Please see <http://www.gnu.org/licenses/agpl-3.0.html> for more details.
+ *
+ *  Licensing for other types of projects:
+ *  Pkcs11Interop is available under the terms of flexible commercial license.
+ *  Please contact JWC s.r.o. at <info@pkcs11interop.net> for more details.
+ */
+
+using System;
+using System.IO;
+
+namespace Net.Pkcs11Interop.Common
+{
+    /// <summary>
+    /// Utility class for runtime platform detection
+    /// </summary>
+    public static class Platform
+    {
+        /// <summary>
+        /// True if runtime platform is Windows
+        /// </summary>
+        private static bool _isWindows = false;
+
+        /// <summary>
+        /// True if runtime platform is Windows
+        /// </summary>
+        public static bool IsWindows
+        {
+            get
+            {
+                DetectPlatform();
+                return _isWindows;
+            }
+        }
+
+        /// <summary>
+        /// True if runtime platform is Linux
+        /// </summary>
+        private static bool _isLinux = false;
+
+        /// <summary>
+        /// True if runtime platform is Linux
+        /// </summary>
+        public static bool IsLinux
+        {
+            get
+            {
+                DetectPlatform();
+                return _isLinux;
+            }
+        }
+
+        /// <summary>
+        /// True if runtime platform is Mac OS X
+        /// </summary>
+        private static bool _isMacOsX = false;
+
+        /// <summary>
+        /// True if runtime platform is Mac OS X
+        /// </summary>
+        public static bool IsMacOsX
+        {
+            get
+            {
+                DetectPlatform();
+                return _isMacOsX;
+            }
+        }
+
+        /// <summary>
+        /// Performs platform detection
+        /// </summary>
+        private static void DetectPlatform()
+        {
+            // Supported platform has already been detected
+            if (_isWindows || _isLinux || _isMacOsX)
+                return;
+
+            int platformId = (int)System.Environment.OSVersion.Platform;
+
+            //   4 - Unix   - Almost everything else than Windows
+            //   6 - MacOSX - Correctly detected (or not) only by newer versions of Mono
+            // 128 - Unix   - Used by a few ancient versions of Mono
+            if (platformId == 4 || platformId == 6 || platformId == 128)
+            {
+                // Note: I don't like the idea of pinvoking uname() function (via mono.posix or directly)
+                //       or executing uname app so let's try this slightly higher level approach
+                if (File.Exists(@"/proc/sys/kernel/ostype"))
+                {
+                    string osType = File.ReadAllText(@"/proc/sys/kernel/ostype");
+                    if (osType.StartsWith("Linux", StringComparison.InvariantCultureIgnoreCase))
+                        _isLinux = true;
+                    else
+                        throw new Exception("This platform (" + osType + ") is not supported");
+                }
+                else if (File.Exists(@"/System/Library/CoreServices/SystemVersion.plist"))
+                {
+                    _isMacOsX = true;
+                }
+                else
+                {
+                    throw new Exception("This platform is not supported");
+                }
+            }
+            else
+            {
+                _isWindows = true;
+            }
+        }
+    }
+}

src/Pkcs11Interop/Common/UnmanagedLibrary.cs
@@ -36,10 +36,16 @@ namespace Net.Pkcs11Interop.Common
 
             IntPtr libraryHandle = IntPtr.Zero;
 
-            PlatformID platformId = System.Environment.OSVersion.Platform;
-            if ((platformId == PlatformID.Unix) || (platformId == PlatformID.MacOSX))
+            if (Platform.IsLinux || Platform.IsMacOsX)
             {
-                libraryHandle = NativeMethods.dlopen(fileName, NativeMethods.RTLD_NOW | NativeMethods.RTLD_LOCAL);
+                int flags = 0;
+
+                if (Platform.IsLinux)
+                    flags = NativeMethods.RTLD_NOW_LINUX | NativeMethods.RTLD_LOCAL_LINUX;
+                else
+                    flags = NativeMethods.RTLD_NOW_MACOSX | NativeMethods.RTLD_LOCAL_MACOSX;
+
+                libraryHandle = NativeMethods.dlopen(fileName, flags);
                 if (libraryHandle == IntPtr.Zero)
                 {
                     IntPtr error = NativeMethods.dlerror();
@@ -68,8 +74,7 @@ namespace Net.Pkcs11Interop.Common
             if (libraryHandle == IntPtr.Zero)
                 throw new ArgumentNullException("libraryHandle");
 
-            PlatformID platformId = System.Environment.OSVersion.Platform;
-            if ((platformId == PlatformID.Unix) || (platformId == PlatformID.MacOSX))
+            if (Platform.IsLinux || Platform.IsMacOsX)
             {
                 if (0 != NativeMethods.dlclose(libraryHandle))
                 {
@@ -103,8 +108,7 @@ namespace Net.Pkcs11Interop.Common
 
             IntPtr functionPointer = IntPtr.Zero;
 
-            PlatformID platformId = System.Environment.OSVersion.Platform;
-            if ((platformId == PlatformID.Unix) || (platformId == PlatformID.MacOSX))
+            if (Platform.IsLinux || Platform.IsMacOsX)
             {
                 NativeMethods.dlerror();
                 functionPointer = NativeMethods.dlsym(libraryHandle, function);

src/Pkcs11Interop/Common/UnmanagedLong.cs
@@ -37,8 +37,7 @@ namespace Net.Pkcs11Interop.Common
                 if (_size != 0)
                     return _size;
 
-                PlatformID platformId = System.Environment.OSVersion.Platform;
-                if ((platformId == PlatformID.Unix) || (platformId == PlatformID.MacOSX))
+                if (Platform.IsLinux || Platform.IsMacOsX)
                 {
                     // CK_ULONG is 4 bytes long on 32-bit Unix and 8 bytes long 64-bit Unix
                     _size = IntPtr.Size;

src/Pkcs11Interop/LowLevelAPI4/Pkcs11.cs
@@ -164,8 +164,7 @@ namespace Net.Pkcs11Interop.LowLevelAPI4
             if ((rv != CKR.CKR_OK) || (functionListPointer == IntPtr.Zero))
                 throw new Pkcs11Exception("C_GetFunctionList", rv);
 
-            PlatformID platformId = System.Environment.OSVersion.Platform;
-            if ((platformId == PlatformID.Unix) || (platformId == PlatformID.MacOSX))
+            if (Platform.IsLinux || Platform.IsMacOsX)
             {
                 // Workaround for MONO on Linux and OS X where marshaling of CK_FUNCTION_LIST is not working correctly
                 CK_FUNCTION_LIST_UNIX functionListUnix = (CK_FUNCTION_LIST_UNIX)UnmanagedMemory.Read(functionListPointer, typeof(CK_FUNCTION_LIST_UNIX));

src/Pkcs11Interop/LowLevelAPI8/Pkcs11.cs
@@ -164,8 +164,7 @@ namespace Net.Pkcs11Interop.LowLevelAPI8
             if ((rv != CKR.CKR_OK) || (functionListPointer == IntPtr.Zero))
                 throw new Pkcs11Exception("C_GetFunctionList", rv);
 
-            PlatformID platformId = System.Environment.OSVersion.Platform;
-            if ((platformId == PlatformID.Unix) || (platformId == PlatformID.MacOSX))
+            if (Platform.IsLinux || Platform.IsMacOsX)
             {
                 // Workaround for MONO on Linux and OS X where marshaling of CK_FUNCTION_LIST is not working correctly
                 CK_FUNCTION_LIST_UNIX functionListUnix = (CK_FUNCTION_LIST_UNIX)UnmanagedMemory.Read(functionListPointer, typeof(CK_FUNCTION_LIST_UNIX));

src/Pkcs11Interop/Pkcs11Interop.csproj
@@ -54,6 +54,7 @@
     <Compile Include="Common\CKU.cs" />
     <Compile Include="Common\CKZ.cs" />
     <Compile Include="Common\ConvertUtils.cs" />
+    <Compile Include="Common\Platform.cs" />
     <Compile Include="Common\Pkcs11Exception.cs" />
     <Compile Include="Common\IMechanismParams.cs" />
     <Compile Include="Common\UnmanagedLong.cs" />