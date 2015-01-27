2bfa82b8acef360afc9ab9ecd84bca9e34c00e0d
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2015-01-27 23:09:50 +0100

    SL: Added Pkcs11Interop.Silverlight.Tests project

---
+++ src/Pkcs11Interop.Silverlight.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pkcs11Interop.Silverlight.Tests")]
+[assembly: AssemblyDescription("Managed .NET wrapper for unmanaged PKCS#11 libraries")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JWC s.r.o.")]
+[assembly: AssemblyProduct("Pkcs11Interop (AGPL)")]
+[assembly: AssemblyCopyright("Copyright © 2012-2015 JWC s.r.o. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65e0c9a3-964c-4442-b695-2a18ceffc3f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
