35d1f69b619b8fffafce73999fde5edca1b7ff49
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2015-03-16 21:55:48 +0100

    Added Pkcs11Interop.iOS project...
    ...that works with Xamarin.iOS INDIE edition or higher.

---
+++ src/Pkcs11Interop.iOS.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("Pkcs11Interop.iOS.Tests")]
+[assembly: AssemblyDescription("Managed .NET wrapper for unmanaged PKCS#11 libraries")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JWC s.r.o.")]
+[assembly: AssemblyProduct("Pkcs11Interop (AGPL)")]
+[assembly: AssemblyCopyright("Copyright © 2012-2015 JWC s.r.o. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
