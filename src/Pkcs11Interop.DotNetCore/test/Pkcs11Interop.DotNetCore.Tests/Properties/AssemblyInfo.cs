d0dca00be5b6c7f9b4b5eafced8c93e6767cd239
Jaroslav Imrich <jimrich@jimrich.sk>
2016-06-25 13:28:09 +0200

    Use dotnet-test-nunit
    as described at
    http://www.alteridem.net/2016/06/18/nunit-3-testing-net-core-rc2/

--- src/Pkcs11Interop.DotNetCore/test/Pkcs11Interop.DotNetCore.Tests/Properties/AssemblyInfo.cs
+++ src/Pkcs11Interop.DotNetCore/test/Pkcs11Interop.DotNetCore.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ecbfaa1-27e1-4f78-b353-5014febbee24")]
+[assembly: Guid("e7f90384-d5e2-4451-bdbe-7836ea107175")]
 
 // Version information for an assembly consists of the following four values:
 //
