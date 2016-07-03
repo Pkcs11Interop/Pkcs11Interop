454f6db6c1c17b68cf3911e02ffdb076f34ebb4b
Jaroslav Imrich <jimrich@jimrich.sk>
2016-07-03 16:44:15 +0200

    Added Android solution created in VS2015

---
+++ src/Pkcs11Interop.Android/Pkcs11Interop.Android.Tests/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using System.Reflection;
+using Xamarin.Android.NUnitLite;
+
+namespace Pkcs11Interop.Android.Tests
+{
+    [Activity(Label = "Pkcs11Interop.Android.Tests", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            AddTest(Assembly.GetExecutingAssembly());
+            base.OnCreate(bundle);
+        }
+    }
+}
+
---
+++ src/Pkcs11Interop.Android/Pkcs11Interop.Android.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pkcs11Interop.Android.Tests")]
+[assembly: AssemblyDescription("Managed .NET wrapper for unmanaged PKCS#11 libraries")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pkcs11Interop")]
+[assembly: AssemblyCopyright("Copyright 2012-2016 The Pkcs11Interop Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
