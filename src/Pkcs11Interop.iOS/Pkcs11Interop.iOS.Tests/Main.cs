7205f5bfef1453ae3938fd80ae307ad62206c3af
Jaroslav Imrich <jimrich@jimrich.sk>
2016-07-09 17:01:19 +0200

    Added iOS solution created in Xamarin Studio 6 on Mac

---
+++ src/Pkcs11Interop.iOS/Pkcs11Interop.iOS.Tests/Main.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace Pkcs11Interop.iOS.Tests
+{
+    public class Application
+    {
+        static void Main (string [] args)
+        {
+            UIApplication.Main(args, null, "UnitTestAppDelegate");
+        }
+    }
+}
---
+++ src/Pkcs11Interop.iOS/Pkcs11Interop.iOS.Tests/UnitTestAppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using Foundation;
+using MonoTouch.NUnit.UI;
+using UIKit;
+
+namespace Pkcs11Interop.iOS.Tests
+{
+    [Register ("UnitTestAppDelegate")]
+    public partial class UnitTestAppDelegate : UIApplicationDelegate
+    {
+        UIWindow window;
+        TouchRunner runner;
+
+        public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+        {
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+            runner = new TouchRunner(window);
+
+            runner.Add(System.Reflection.Assembly.GetExecutingAssembly());
+
+            window.RootViewController = new UINavigationController(runner.GetViewController());
+            window.MakeKeyAndVisible();
+            return true;
+        }
+    }
+}
