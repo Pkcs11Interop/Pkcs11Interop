8016377911238d03811086e09ddd84b11ed20b3c
Jaroslav Imrich <jimrich@jimrich.sk>
2017-04-17 14:48:42 +0200

    Prefer VS2017 over VS2015 in build scripts

--- build/build-net45.bat
+++ build/build-net45.bat
@@ -1,7 +1,20 @@
 @setlocal
 
-@rem Initialize build environment of Visual Studio 2015
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
+@rem Initialize Visual Studio build environment:
+@rem - Visual Studio 2017 Community/Professional/Enterprise is the preferred option
+@rem - Visual Studio 2015 is the fallback option (which might or might not work)
+@set tools=
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@if not defined tools goto :error
+call %tools%
+@echo on
 
 @rem Delete output directory
 rmdir /S /Q net45
