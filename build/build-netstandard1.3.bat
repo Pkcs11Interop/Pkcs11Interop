aa6431f765d7f02e95846a6bef0dbca458cb994a
Jaroslav Imrich <jimrich@jimrich.sk>
2017-05-30 22:21:43 +0200

    Adjusted DotNetCore build script

--- build/build-netstandard1.3.bat
+++ build/build-netstandard1.3.bat
@@ -4,15 +4,10 @@
 rmdir /S /Q netstandard1.3
 
 @rem Clean project
-rmdir /S /Q ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\bin
-rmdir /S /Q ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\obj
-del ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\project.lock.json
-rmdir /S /Q ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\bin
-rmdir /S /Q ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\obj
-del ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\project.lock.json
+dotnet clean ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\ --configuration Release || goto :error
+dotnet clean ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\ --configuration Release || goto :error
 
 @rem Build project
-copy /Y ..\src\Pkcs11Interop.DotNetCore\global.json .
 dotnet restore ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\ || goto :error
 dotnet build ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\ --configuration Release || goto :error
 dotnet restore ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\ || goto :error
