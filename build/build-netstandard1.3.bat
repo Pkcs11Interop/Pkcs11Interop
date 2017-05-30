729bfa5f6ae66cd32260b397f24bb3e1fe32d36d
Jaroslav Imrich <jimrich@jimrich.sk>
2017-05-31 00:29:27 +0200

    Adapt to Core SDK 1.0.4

--- build/build-netstandard1.3.bat
+++ build/build-netstandard1.3.bat
@@ -15,14 +15,12 @@ dotnet build ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\ --con
 dotnet restore ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\ || goto :error
 dotnet build ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\ --configuration Release || goto :error
 
-tree /a /f ..
-
 @rem Copy result to output directory
 mkdir netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\bin\Release\netstandard1.3\Pkcs11Interop.DotNetCore.dll netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\bin\Release\netstandard1.3\Pkcs11Interop.DotNetCore.xml netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\bin\Release\netstandard1.3\Pkcs11Interop.DotNetCore.StrongName.dll netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\bin\Release\netstandard1.3\Pkcs11Interop.DotNetCore.StrongName.xml netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\bin\Release\Pkcs11Interop.DotNetCore.dll netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\bin\Release\Pkcs11Interop.DotNetCore.xml netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\bin\Release\Pkcs11Interop.DotNetCore.StrongName.dll netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\bin\Release\Pkcs11Interop.DotNetCore.StrongName.xml netstandard1.3 || goto :error
 
 @echo *** BUILD NETSTANDARD1.3 SUCCESSFUL ***
 @endlocal
