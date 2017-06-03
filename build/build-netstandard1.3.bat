e0c0cab81fdfd3ad9eca75216a2e7d1f7140ba9c
Jaroslav Imrich <jimrich@jimrich.sk>
2017-06-03 23:31:23 +0200

    Adjust build scripts to use new solution

--- build/build-netstandard1.3.bat
+++ build/build-netstandard1.3.bat
@@ -16,29 +16,29 @@ call %tools%
 rmdir /S /Q netstandard1.3
 
 @rem Clean project
-msbuild ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\Pkcs11Interop.DotNetCore.csproj ^
+msbuild ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.csproj ^
 /p:Configuration=Release /p:Platform=AnyCPU /target:Clean || goto :error
-msbuild ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\Pkcs11Interop.DotNetCore.StrongName.csproj ^
+msbuild ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.StrongName\Pkcs11Interop.NetStandard.StrongName.csproj ^
 /p:Configuration=Release /p:Platform=AnyCPU /target:Clean || goto :error
 
 @rem Restore dependencies
-msbuild ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\Pkcs11Interop.DotNetCore.csproj ^
+msbuild ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.csproj ^
 /p:Configuration=Release /p:Platform=AnyCPU /target:Restore || goto :error
-msbuild ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\Pkcs11Interop.DotNetCore.StrongName.csproj ^
+msbuild ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.StrongName\Pkcs11Interop.NetStandard.StrongName.csproj ^
 /p:Configuration=Release /p:Platform=AnyCPU /target:Restore || goto :error
 
 @rem Build project
-msbuild ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\Pkcs11Interop.DotNetCore.csproj ^
+msbuild ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.csproj ^
 /p:Configuration=Release /p:Platform=AnyCPU /target:Build || goto :error
-msbuild ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\Pkcs11Interop.DotNetCore.StrongName.csproj ^
+msbuild ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.StrongName\Pkcs11Interop.NetStandard.StrongName.csproj ^
 /p:Configuration=Release /p:Platform=AnyCPU /target:Build || goto :error
 
 @rem Copy result to output directory
 mkdir netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\bin\Release\Pkcs11Interop.DotNetCore.dll netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore\bin\Release\Pkcs11Interop.DotNetCore.xml netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\bin\Release\Pkcs11Interop.DotNetCore.StrongName.dll netstandard1.3 || goto :error
-copy ..\src\Pkcs11Interop.DotNetCore\src\Pkcs11Interop.DotNetCore.StrongName\bin\Release\Pkcs11Interop.DotNetCore.StrongName.xml netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard\bin\Release\Pkcs11Interop.NetStandard.dll netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard\bin\Release\Pkcs11Interop.NetStandard.xml netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.StrongName\bin\Release\Pkcs11Interop.NetStandard.StrongName.dll netstandard1.3 || goto :error
+copy ..\src\Pkcs11Interop.NetStandard\Pkcs11Interop.NetStandard.StrongName\bin\Release\Pkcs11Interop.NetStandard.StrongName.xml netstandard1.3 || goto :error
 
 @echo *** BUILD NETSTANDARD1.3 SUCCESSFUL ***
 @endlocal
