5069ca5501399b24aced0cf370283f4a01c641ff
Jaroslav Imrich <jimrich@jimrich.sk>
2017-06-04 08:57:08 +0200

    Manually adjusted csproj files
    * List of linked source files can be diffed with other solutions
    * AssemblyInfo.cs is used instead of automatically generating values
    * Files unlisted in csproj are not automatically included in compilation
    process

---
+++ src/Pkcs11Interop.NetStandard/Pkcs11Interop.DotNetCore.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pkcs11Interop.DotNetCore.Tests")]
+[assembly: AssemblyDescription("Managed .NET wrapper for unmanaged PKCS#11 libraries")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pkcs11Interop")]
+[assembly: AssemblyCopyright("Copyright 2012-2017 The Pkcs11Interop Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e7f90384-d5e2-4451-bdbe-7836ea107175")]
+[assembly: AssemblyVersion("3.3.0")]
+[assembly: AssemblyFileVersion("3.3.0")]
+[assembly: AssemblyInformationalVersion("3.3.0")]
---
+++ src/Pkcs11Interop.NetStandard/Pkcs11Interop.NetStandard/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pkcs11Interop.NetStandard")]
+[assembly: AssemblyDescription("Managed .NET wrapper for unmanaged PKCS#11 libraries")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pkcs11Interop")]
+[assembly: AssemblyCopyright("Copyright 2012-2017 The Pkcs11Interop Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("af7dfa22-9f88-4497-bf4f-ddd2eda16fb5")]
+[assembly: AssemblyVersion("3.3.0")]
+[assembly: AssemblyFileVersion("3.3.0")]
+[assembly: AssemblyInformationalVersion("3.3.0")]
