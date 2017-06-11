d2361f307f2c55365c6c1cc72a6b5d8a1693afa7
Jaroslav Imrich <jimrich@jimrich.sk>
2017-06-11 11:07:41 +0200

    Introduce AppType and InitType enums

---
+++ src/Pkcs11Interop/Pkcs11Interop/Common/AppType.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *  Copyright 2012-2017 The Pkcs11Interop Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ *  Written for the Pkcs11Interop project by:
+ *  Jaroslav IMRICH <jimrich@jimrich.sk>
+ */
+
+namespace Net.Pkcs11Interop.Common
+{
+    /// <summary>
+    /// Type of application that will be using PKCS#11 library
+    /// </summary>
+    public enum AppType
+    {
+        /// <summary>
+        /// Recommended option: PKCS#11 library will be used from multi-threaded application and needs to perform locking with native OS threading model (CKF_OS_LOCKING_OK)
+        /// </summary>
+        MultiThreaded,
+
+        /// <summary>
+        /// PKCS#11 library will be used from single-threaded application and does not need to perform any kind of locking
+        /// </summary>
+        SingleThreaded
+    }
+}
---
+++ src/Pkcs11Interop/Pkcs11Interop/Common/InitType.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *  Copyright 2012-2017 The Pkcs11Interop Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ *  Written for the Pkcs11Interop project by:
+ *  Jaroslav IMRICH <jimrich@jimrich.sk>
+ */
+
+namespace Net.Pkcs11Interop.Common
+{
+    /// <summary>
+    /// Source of PKCS#11 function pointers
+    /// </summary>
+    public enum InitType
+    {
+        /// <summary>
+        /// Recommended option: PKCS#11 function pointers will be acquired with single call of C_GetFunctionList function
+        /// </summary>
+        WithFunctionList,
+
+        /// <summary>
+        /// PKCS#11 function pointers will be acquired with multiple calls of GetProcAddress or dlsym function
+        /// </summary>
+        WithoutFunctionList
+    }
+}
