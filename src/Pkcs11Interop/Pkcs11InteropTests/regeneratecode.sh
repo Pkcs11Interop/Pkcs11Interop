b3aa2615d4afbd561819d8746ae9f5697c1db1c2
Jaroslav Imrich <jimrich@jimrich.sk>
2018-02-16 23:27:43 +0100

    Added test code generation script
    Generates tests for all LowLevelAPIs from LowLevelAPI41

---
+++ src/Pkcs11Interop/Pkcs11InteropTests/regeneratecode.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+API=LowLevelAPI40
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r LowLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/Net.Pkcs11Interop.LowLevelAPI41/Net.Pkcs11Interop.'"$API"'/' $file
+	sed -i -e 's/Net.Pkcs11Interop.Tests.LowLevelAPI41/Net.Pkcs11Interop.Tests.'"$API"'/' $file
+	sed -i -e 's/Settings.InitArgs41/Settings.InitArgs40/' $file
+done
+sed -i -e 's/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 0)/' ./$API/Helpers.cs
+
+API=LowLevelAPI80
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r LowLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/Net.Pkcs11Interop.LowLevelAPI41/Net.Pkcs11Interop.'"$API"'/' $file
+	sed -i -e 's/Net.Pkcs11Interop.Tests.LowLevelAPI41/Net.Pkcs11Interop.Tests.'"$API"'/' $file
+	sed -i -e 's/Settings.InitArgs41/Settings.InitArgs80/' $file
+	sed -i -e 's/using NativeLong = System.UInt32;/using NativeLong = System.UInt64;/' $file
+done
+sed -i -e 's/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)/(Platform.UnmanagedLongSize != 8 || Platform.StructPackingSize != 0)/' ./$API/Helpers.cs
+
+API=LowLevelAPI81
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r LowLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/Net.Pkcs11Interop.LowLevelAPI41/Net.Pkcs11Interop.'"$API"'/' $file
+	sed -i -e 's/Net.Pkcs11Interop.Tests.LowLevelAPI41/Net.Pkcs11Interop.Tests.'"$API"'/' $file
+	sed -i -e 's/Settings.InitArgs41/Settings.InitArgs81/' $file
+	sed -i -e 's/using NativeLong = System.UInt32;/using NativeLong = System.UInt64;/' $file
+done
+sed -i -e 's/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)/(Platform.UnmanagedLongSize != 8 || Platform.StructPackingSize != 1)/' ./$API/Helpers.cs
