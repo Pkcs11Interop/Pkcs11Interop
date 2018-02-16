e14f492bc14a5fc1628a69e7b3e4ca5540ae4105
Jaroslav Imrich <jimrich@jimrich.sk>
2018-02-16 23:03:38 +0100

    Added code generation script
    Generates all LowLevelAPIs from LowLevelAPI41

---
+++ src/Pkcs11Interop/Pkcs11Interop/regeneratecode.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+API=LowLevelAPI40
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r LowLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/Net.Pkcs11Interop.LowLevelAPI41/Net.Pkcs11Interop.'"$API"'/' $file
+	sed -i -e 's/Pack = 1/Pack = 0/' $file
+done
+
+API=LowLevelAPI80
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r LowLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/Net.Pkcs11Interop.LowLevelAPI41/Net.Pkcs11Interop.'"$API"'/' $file
+	sed -i -e 's/using NativeLong = System.UInt32;/using NativeLong = System.UInt64;/' $file
+	sed -i -e 's/Pack = 1/Pack = 0/' $file
+done
+sed -i -e 's/#if true \/\/ DO NOT REMOVE - UInt32/#if false \/\/ DO NOT REMOVE - UInt32/' ./$API/NativeLongUtils.cs
+sed -i -e 's/#if false \/\/ DO NOT REMOVE - UInt64/#if true \/\/ DO NOT REMOVE - UInt64/' ./$API/NativeLongUtils.cs
+
+API=LowLevelAPI81
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r LowLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/Net.Pkcs11Interop.LowLevelAPI41/Net.Pkcs11Interop.'"$API"'/' $file
+	sed -i -e 's/using NativeLong = System.UInt32;/using NativeLong = System.UInt64;/' $file
+done
+sed -i -e 's/#if true \/\/ DO NOT REMOVE - UInt32/#if false \/\/ DO NOT REMOVE - UInt32/' ./$API/NativeLongUtils.cs
+sed -i -e 's/#if false \/\/ DO NOT REMOVE - UInt64/#if true \/\/ DO NOT REMOVE - UInt64/' ./$API/NativeLongUtils.cs
