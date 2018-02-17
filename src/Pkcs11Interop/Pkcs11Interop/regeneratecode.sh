bfb2a4e09eb4a3f86fb07b3f6452a6169b8d162a
Jaroslav Imrich <jimrich@jimrich.sk>
2018-02-17 18:35:06 +0100

    Updated code generation script
    Generates all HighLevelAPIs from HighLevelAPI41

--- src/Pkcs11Interop/Pkcs11Interop/regeneratecode.sh
+++ src/Pkcs11Interop/Pkcs11Interop/regeneratecode.sh
@@ -39,3 +39,38 @@ for file in $files; do
 done
 sed -i -e 's/#if true \/\/ DO NOT REMOVE - UInt32/#if false \/\/ DO NOT REMOVE - UInt32/' ./$API/NativeLongUtils.cs
 sed -i -e 's/#if false \/\/ DO NOT REMOVE - UInt64/#if true \/\/ DO NOT REMOVE - UInt64/' ./$API/NativeLongUtils.cs
+
+API=HighLevelAPI40
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r HighLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/HighLevelAPI41/'"$API"'/' $file
+	sed -i -e 's/LowLevelAPI41/LowLevelAPI40/' $file
+done
+
+API=HighLevelAPI80
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r HighLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/HighLevelAPI41/'"$API"'/' $file
+	sed -i -e 's/LowLevelAPI41/LowLevelAPI80/' $file
+	sed -i -e 's/using NativeULong = System.UInt32;/using NativeULong = System.UInt64;/' $file
+done
+
+API=HighLevelAPI81
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r HighLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/HighLevelAPI41/'"$API"'/' $file
+	sed -i -e 's/LowLevelAPI41/LowLevelAPI81/' $file
+	sed -i -e 's/using NativeULong = System.UInt32;/using NativeULong = System.UInt64;/' $file
+done
