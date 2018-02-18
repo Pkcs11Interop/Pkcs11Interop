54d96776d1335a0d13868b397bc4c80c3d7d07b3
Jaroslav Imrich <jimrich@jimrich.sk>
2018-02-18 21:26:00 +0100

    Updated test code generation script
    Generates tests for all HighLevelAPIs from HighLevelAPI41

--- src/Pkcs11Interop/Pkcs11InteropTests/regeneratecode.sh
+++ src/Pkcs11Interop/Pkcs11InteropTests/regeneratecode.sh
@@ -42,3 +42,41 @@ for file in $files; do
 	sed -i -e 's/using NativeULong = System.UInt32;/using NativeULong = System.UInt64;/' $file
 done
 sed -i -e 's/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)/(Platform.UnmanagedLongSize != 8 || Platform.StructPackingSize != 1)/' ./$API/Helpers.cs
+
+API=HighLevelAPI40
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r HighLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/HighLevelAPI41/'"$API"'/' $file
+	sed -i -e 's/LowLevelAPI41/LowLevelAPI40/' $file
+done
+sed -i -e 's/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 0)/' ./$API/Helpers.cs
+
+API=HighLevelAPI80
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r HighLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/HighLevelAPI41/'"$API"'/' $file
+	sed -i -e 's/LowLevelAPI41/LowLevelAPI80/' $file
+	sed -i -e 's/using NativeULong = System.UInt32;/using NativeULong = System.UInt64;/' $file
+done
+sed -i -e 's/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)/(Platform.UnmanagedLongSize != 8 || Platform.StructPackingSize != 0)/' ./$API/Helpers.cs
+
+API=HighLevelAPI81
+echo "Regenerating $API"
+rm -rf $API || :
+cp -r HighLevelAPI41 $API
+files=`find ./$API -type f`
+for file in $files; do
+	echo "  Processing $file"
+	sed -i -e 's/HighLevelAPI41/'"$API"'/' $file
+	sed -i -e 's/LowLevelAPI41/LowLevelAPI81/' $file
+	sed -i -e 's/using NativeULong = System.UInt32;/using NativeULong = System.UInt64;/' $file
+done
+sed -i -e 's/(Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)/(Platform.UnmanagedLongSize != 8 || Platform.StructPackingSize != 1)/' ./$API/Helpers.cs
