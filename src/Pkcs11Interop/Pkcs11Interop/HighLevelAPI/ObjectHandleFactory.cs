3332399fa56f4055596ee94efbab9ec844a35021
Jaroslav Imrich <jimrich@jimrich.sk>
2018-03-18 12:11:11 +0100

    Added ObjectHandleFactory

---
+++ src/Pkcs11Interop/Pkcs11Interop/HighLevelAPI/ObjectHandleFactory.cs
@@ -0,0 +1,104 @@
+﻿/*
+ *  Copyright 2012-2017 The Pkcs11Interop Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ *  Written for the Pkcs11Interop project by:
+ *  Jaroslav IMRICH <jimrich@jimrich.sk>
+ */
+
+using Net.Pkcs11Interop.Common;
+
+namespace Net.Pkcs11Interop.HighLevelAPI
+{
+    /// <summary>
+    /// Factory for creation of IObjectHandle instances
+    /// </summary>
+    public class ObjectHandleFactory : IObjectHandleFactory
+    {
+        #region Static instance of the factory
+
+        /// <summary>
+        /// Ready to use static instance of the factory
+        /// </summary>
+        private static ObjectHandleFactory _instance = null;
+
+        /// <summary>
+        /// Ready to use static instance of the factory
+        /// </summary>
+        public static ObjectHandleFactory Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Static constructor that initializes static instance of the factory
+        /// </summary>
+        static ObjectHandleFactory()
+        {
+            _instance = new ObjectHandleFactory();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Platform specific factory for creation of IObjectHandle instances
+        /// </summary>
+        private IObjectHandleFactory _factory = null;
+
+        /// <summary>
+        /// Initializes a new instance of the ObjectHandleFactory class
+        /// </summary>
+        public ObjectHandleFactory()
+        {
+            if (Platform.UnmanagedLongSize == 4)
+            {
+                if (Platform.StructPackingSize == 0)
+                    _factory = new HighLevelAPI40.ObjectHandleFactory();
+                else
+                    _factory = new HighLevelAPI41.ObjectHandleFactory();
+            }
+            else
+            {
+                if (Platform.StructPackingSize == 0)
+                    _factory = new HighLevelAPI80.ObjectHandleFactory();
+                else
+                    _factory = new HighLevelAPI81.ObjectHandleFactory();
+            }
+        }
+
+        /// <summary>
+        /// Creates object identifier with CK_INVALID_HANDLE value
+        /// </summary>
+        /// <returns>Token-specific identifier for an object</returns>
+        public IObjectHandle CreateObjectHandle()
+        {
+            return _factory.CreateObjectHandle();
+        }
+
+        /// <summary>
+        /// Creates object identifier with provided value
+        /// </summary>
+        /// <param name="objectId">PKCS#11 handle of object</param>
+        /// <returns>Token-specific identifier for an object</returns>
+        public IObjectHandle CreateObjectHandle(ulong objectId)
+        {
+            return _factory.CreateObjectHandle(objectId);
+        }
+    }
+}
