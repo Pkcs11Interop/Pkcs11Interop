4c22b9ac3deabfea2fce8b26964ad4f706caabc7
Jaroslav Imrich <jimrich@jimrich.sk>
2019-01-13 01:59:51 +0100

    Code changes required to support Xamarin.Mac

--- src/Pkcs11Interop/Pkcs11Interop.Mock/LowLevelAPI81/MockDelegates.cs
+++ src/Pkcs11Interop/Pkcs11Interop.Mock/LowLevelAPI81/MockDelegates.cs
@@ -47,13 +47,19 @@ namespace Net.Pkcs11Interop.Mock.LowLevelAPI81
         /// </summary>
         private static class NativeMethods
         {
-            [DllImport("__Internal", EntryPoint = "C_GetUnmanagedStructSizeList", CallingConvention = CallingConvention.Cdecl)]
+#if XAMARINMAC2_0
+            private const string _dllName = "NonExistingLibrary";
+#else
+            private const string _dllName = "__Internal";
+#endif
+
+            [DllImport(_dllName, EntryPoint = "C_GetUnmanagedStructSizeList", CallingConvention = CallingConvention.Cdecl)]
             internal static extern NativeULong C_GetUnmanagedStructSizeList(NativeULong[] sizeList, ref NativeULong count);
 
-            [DllImport("__Internal", EntryPoint = "C_EjectToken", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport(_dllName, EntryPoint = "C_EjectToken", CallingConvention = CallingConvention.Cdecl)]
             internal static extern NativeULong C_EjectToken(NativeULong slotId);
 
-            [DllImport("__Internal", EntryPoint = "C_InteractiveLogin", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport(_dllName, EntryPoint = "C_InteractiveLogin", CallingConvention = CallingConvention.Cdecl)]
             internal static extern NativeULong C_InteractiveLogin(NativeULong session);
         }
 
