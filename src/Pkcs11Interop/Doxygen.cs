c4c41b75875dc8256b2816ba300bf639c83179df
Jaroslav Imrich <jimrich@jimrich.sk>
2019-05-01 14:00:25 +0200

    Updated doxygen documentation

--- src/Pkcs11Interop/Doxygen.cs
+++ src/Pkcs11Interop/Doxygen.cs
@@ -2,7 +2,7 @@
  * 
  * \section overview Overview
  * 
- * <a href="http://www.pkcs11interop.net">Pkcs11interop</a> forms a bridge between the unmanaged ANSI C and managed .NET worlds. It loads unmanaged PKCS#11 library provided by the cryptographic device vendor and makes its functions accessible to .NET application.
+ * <a href="https://pkcs11interop.net">Pkcs11interop</a> forms a bridge between the unmanaged ANSI C and managed .NET worlds. It loads unmanaged PKCS#11 library provided by the cryptographic device vendor and makes its functions accessible to .NET application.
  * 
  * Pkcs11interop uses `System.Runtime.InteropServices` to define platform invoke methods for unmanaged PKCS#11 API and specifies how data is marshaled between managed and unmanaged memory.
  * 
@@ -31,7 +31,7 @@
  *    
  * \section zeros-and-ones-in-the-api-s Zeros and ones in the API-s
  * 
- * PKCS#11 specifications v2.01 - v2.30 all vaguely state:
+ * <a href="https://github.com/Pkcs11Interop/PKCS11-SPECS">PKCS#11 specifications</a> v2.01 - v2.30 all vaguely state:
  * 
  * <blockquote>Cryptoki structures are packed to occupy as little space as is possible. In particular, on the Win32 and Win16 platforms, Cryptoki structures should be packed with 1-byte alignment. In a UNIX environment, it may or may not be necessary (or even possible) to alter the byte-alignment of structures.</blockquote>
  * 
@@ -42,7 +42,9 @@
  *
  * \section recommended-api-s Recommended API-s
  * 
- * `Net.Pkcs11Interop.HighLevelAPI` automagically uses correct set of platform dependent API-s and therefore is recommended API for most use cases.
+ * Up to version 4, Pkcs11Interop used individual classes located in `Net.Pkcs11Interop.HighLevelAPI` namespace that automatically used the correct set of platform dependent API-s.
+ * 
+ * Since version 5, Pkcs11Interop uses <a href="https://en.wikipedia.org/wiki/Factory_method_pattern">factory method pattern</a> implemented by `Net.Pkcs11Interop.HighLevelAPI.Pkcs11InteropFactories` class that automatically instantiates the correct set of platform dependent API-s.
  */
 
 
@@ -52,14 +54,21 @@
  * 
  * 
  * \namespace Net.Pkcs11Interop.Common
- * \brief Components shared by all the LowLevelAPIs and HighLevelAPIs
+ * \brief Components shared by all the LowLevelAPI-s and HighLevelAPI-s
+ * 
+ * 
+ * \namespace Net.Pkcs11Interop.Logging
+ * \brief Logger components shared by all the LowLevelAPI-s and HighLevelAPI-s
  * 
  * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI
  * \brief High level .NET friendly API recommended for multiplatform development
  * 
+ * \namespace Net.Pkcs11Interop.HighLevelAPI.Factories
+ * \brief Interfaces for factories implemented by individual HighLevelAPI-s
+ * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI.MechanismParams
- * \brief Classes that can hold parameters for various mechanisms usable by HighLevelAPI
+ * \brief Interfaces for mechanism parameters implemented by individual HighLevelAPI-s
  * 
  * 
  * \namespace Net.Pkcs11Interop.LowLevelAPI40
@@ -70,6 +79,9 @@
  * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI40
  * \brief High level .NET friendly API for platforms where C 'long' type is 4 bytes long (Win32, Win64 and Unix32) and structs are packed with the default byte alignment
+ * 
+ * \namespace Net.Pkcs11Interop.HighLevelAPI40.Factories
+ * \brief Factory classes that create objects usable by HighLevelAPI40
  * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI40.MechanismParams
  * \brief Classes that can hold parameters for various mechanisms usable by HighLevelAPI40
@@ -84,6 +96,9 @@
  * \namespace Net.Pkcs11Interop.HighLevelAPI41
  * \brief High level .NET friendly API for platforms where C 'long' type is 4 bytes long (Win32, Win64 and Unix32) and structs are packed with 1-byte alignment
  * 
+ * \namespace Net.Pkcs11Interop.HighLevelAPI41.Factories
+ * \brief Factory classes that create objects usable by HighLevelAPI41
+ * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI41.MechanismParams
  * \brief Classes that can hold parameters for various mechanisms usable by HighLevelAPI41
  * 
@@ -97,6 +112,9 @@
  * \namespace Net.Pkcs11Interop.HighLevelAPI80
  * \brief High level .NET friendly API for platforms where C 'long' type is 8 bytes long (Unix64) and structs are packed with the default byte alignment
  * 
+ * \namespace Net.Pkcs11Interop.HighLevelAPI80.Factories
+ * \brief Factory classes that create objects usable by HighLevelAPI80
+ * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI80.MechanismParams
  * \brief Classes that can hold parameters for various mechanisms usable by HighLevelAPI80
  * 
@@ -109,6 +127,9 @@
  * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI81
  * \brief High level .NET friendly API for platforms where C 'long' type is 8 bytes long (Unix64) and structs are packed with 1-byte alignment
+ * 
+ * \namespace Net.Pkcs11Interop.HighLevelAPI81.Factories
+ * \brief Factory classes that create objects usable by HighLevelAPI81
  * 
  * \namespace Net.Pkcs11Interop.HighLevelAPI81.MechanismParams
  * \brief Classes that can hold parameters for various mechanisms usable by HighLevelAPI81
@@ -144,8 +165,6 @@
  * \example HighLevelAPI/_25_DeriveKeyTest.cs
  * \example HighLevelAPI/_26_LegacyParallelFunctionsTest.cs
  * \example HighLevelAPI/_27_Pkcs11UriUtilsTest.cs
- * \example HighLevelAPI/_28_Pkcs11ClassExtensionTest.cs
- * \example HighLevelAPI/_29_SlotClassExtensionTest.cs
- * \example HighLevelAPI/_30_SessionClassExtensionTest.cs
+ * \example HighLevelAPI/_28_VendorExtensionsTest.cs
  * \example HighLevelAPI/Helpers.cs
  */
