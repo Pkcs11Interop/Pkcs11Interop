39a77d51fadb37d47300cd58886583d5ced09b3c
Jaroslav Imrich <jimrich@jimrich.sk>
2021-01-31 17:13:41 +0100

    Enable building with VS2019

--- build/nuget-build.bat
+++ build/nuget-build.bat
@@ -4,13 +4,19 @@
 set workingdir=%~dp0
 set outputdir=%workingdir%nuget-unsigned
 
-@rem Initialize build environment of Visual Studio 2017 Community/Professional/Enterprise
+@rem Initialize build environment of Visual Studio 2017 or 2019 Community/Professional/Enterprise
 @set tools=
 @set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
 @if exist %tmptools% set tools=%tmptools%
 @set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsMSBuildCmd.bat"
 @if exist %tmptools% set tools=%tmptools%
 @set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
 @if exist %tmptools% set tools=%tmptools%
 @if not defined tools goto :error
 call %tools%
