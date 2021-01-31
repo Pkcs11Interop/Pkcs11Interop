00f6f3e624e3444027bb94bf9fef59b40c6081b9
Jaroslav Imrich <jimrich@jimrich.sk>
2021-01-31 17:27:12 +0100

    Updated year in copyrights

--- src/Pkcs11Interop/LowLevelAPI40/MechanismParams/CK_RSA_AES_KEY_WRAP_PARAMS.cs
+++ src/Pkcs11Interop/LowLevelAPI40/MechanismParams/CK_RSA_AES_KEY_WRAP_PARAMS.cs
@@ -1,5 +1,5 @@
 ﻿/*
- *  Copyright 2012-2020 The Pkcs11Interop Project
+ *  Copyright 2012-2021 The Pkcs11Interop Project
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
