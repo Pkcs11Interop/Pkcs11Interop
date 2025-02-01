e59fdc47619392b069aba2781a9fba3b69951f72
Jaroslav Imrich <jimrich@jimrich.sk>
2025-02-01 22:34:14 +0100

    Updated year in copyrights

--- src/Pkcs11Interop.Tests/LowLevelAPI41/_15_MechanismTest.cs
+++ src/Pkcs11Interop.Tests/LowLevelAPI41/_15_MechanismTest.cs
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2012-2024 The Pkcs11Interop Project
+ *  Copyright 2012-2025 The Pkcs11Interop Project
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
